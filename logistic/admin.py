0c64bdb4de1c8b53413c9c09c6ece7ad653a924d
katerina-arkhipenkova <katerina.arkhipenkova@yandex.ru>
2022-10-27 22:15:37 +0300

    -

--- logistic/admin.py
+++ logistic/admin.py
@@ -1,3 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Product, Stock, StockProduct
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    pass
+
